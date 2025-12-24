0d7dbf5e554264ecb01ea090c71d2ccc7a5e856f
Jason <182464121+latoiste@users.noreply.github.com>
2025-12-24 21:34:33 +0700

    add walking detection

--- lib/services/device_listener.dart
+++ lib/services/device_listener.dart
@@ -1,10 +1,20 @@
+import 'dart:async';
+
 import 'package:bifind_app/models/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:pedometer_plus/pedometer_plus.dart';
 
 class DeviceListener extends ChangeNotifier {
   final List<DeviceInfo> registeredDevice = [];
   final List<DeviceIdentifier> registeredId = [];
+
+  StreamSubscription<StepStatus>? _pedometerSub;
+  StreamSubscription<List<ScanResult>>? _bleSub;
+
+  DeviceListener() {
+    _startMovementDetection();
+  }
 
   void registerDevice(DeviceInfo device) {
     if (registeredId.contains(device.id)) return;
@@ -12,5 +22,24 @@ class DeviceListener extends ChangeNotifier {
     registeredDevice.add(device);
     registeredId.add(device.id);
     notifyListeners();
+  }
+
+  void _startMovementDetection() {
+    _pedometerSub = Pedometer().stepStatusStream().listen(
+      (status) => status == StepStatus.walking ? _startListening() : _stopListening()
+    );
+  }
+
+  void _startListening() {}
+
+  void _stopListening() {
+    _bleSub?.cancel();
+  }
+
+  @override
+  void dispose() {
+    _bleSub?.cancel();
+    _pedometerSub?.cancel();
+    super.dispose();
   }
 }
